d1ef89e2265c1c241db58319eff97bed1cc84988
@rijswij2 <jaden.van.rijswijk@hva.nl>
2023-05-01 00:22:54 +0200

    Fixed semantic analyser now fully working

--- src/SemanticAnalyser/SemanticAnalyser.java
+++ src/SemanticAnalyser/SemanticAnalyser.java
@@ -4,7 +4,9 @@ import LexicalAnalyser.Token;
 import LexicalAnalyser.Token.TokenType;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class SemanticAnalyser {
     private final List<Token> tokens;
@@ -14,16 +16,30 @@ public class SemanticAnalyser {
         this.tokens = tokens;
     }
 
+    private HashMap<String, Integer> getDeclaredVars() {
+        HashMap<String, Integer> tokenMap = new HashMap<>();
+        IntStream.range(0, tokens.size()-1)
+                .filter(i -> tokens.get(i).getType() == TokenType.LET)
+                .forEach(i -> tokenMap.put(tokens.get(i).getValue(), i));
+
+        return tokenMap;
+    }
+
+    private List<Token> getUsedVars() {
+        return tokens.stream().filter(token -> token.getType() == TokenType.VAR).toList();
+    }
+
     public void analyse() {
-        for (int i=0; i<tokens.size(); i++) {
-            Token token = tokens.get(i);
-            if (token.getType() == TokenType.LET) {
-                Token nextToken = tokens.get(i+1);
-                if (nextToken.getType() == TokenType.EQUALS_OPERATOR) {
-                    delcaredVars.add(token);
-                } else if (!delcaredVars.contains(token)) {
-                        throw new SemanticException("Variable " + token + " not declared");
-                }
+        HashMap<String, Integer> declared_vars = getDeclaredVars();
+
+        for (Token token : getUsedVars()) {
+            String tokenValue = token.getValue();
+
+            if (!declared_vars.containsKey(tokenValue)) {
+                throw new SemanticException("Semantic error: Variable " + tokenValue + " is not declared");
+            }
+            else if (declared_vars.get(tokenValue) > tokens.indexOf(token)) {
+                throw new SemanticException("Semantic error: Variable " + tokenValue + " is used before declaration");
             }
         }
     }
